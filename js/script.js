5954fd9213f73c0cb177d5dc574b8df88adb80e9
xelbott <xelbott@gmail.com>
2017-09-08 04:04:28 +0600

    Вёрстка контента

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var modalWindow = document.querySelector(".modal-window");
+var modalWindowControl = document.querySelector(".modal-window-control");
+var arrivalDate = modalWindow.querySelector("[name=arrival-date]");
+modalWindow.classList.add("modal-window-hide");
+modalWindowControl.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	modalWindow.classList.toggle("modal-window-hide");
+	arrivalDate.focus();
+});
