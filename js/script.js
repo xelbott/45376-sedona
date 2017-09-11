6f19162d749ceb832fa174d267563e2ad6525912
xelbott <xelbott@gmail.com>
2017-09-11 17:10:56 +0600

    Анимация формы при ошибке

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 var modalWindow = document.querySelector(".modal-window");
 var modalWindowControl = document.querySelector(".modal-window-control");
+var searchForm = modalWindow.querySelector(".search");
 var arrivalDate = modalWindow.querySelector("[name=arrival-date]");
+var departureDate = modalWindow.querySelector("[name=departure-date]");
+var adultsNumber = modalWindow.querySelector("[name=adults-number]");
+var childrenNumber = modalWindow.querySelector("[name=children-number]");
+var storageAdults = localStorage.getItem("adults");
+var storageChildren = localStorage.getItem("children");
 modalWindow.classList.add("modal-window-hide");
+
 modalWindowControl.addEventListener("click", function(evt) {
 	evt.preventDefault();
+	if (storageAdults) {
+		adultsNumber.value = storageAdults;
+	}
+	if (storageChildren) {
+		childrenNumber.value = storageChildren;
+	}
 	modalWindow.classList.toggle("modal-window-hide");
+	modalWindow.classList.remove("modal-window-error");
 });
+
+searchForm.addEventListener("submit", function(evt) {
+	if (!arrivalDate.value || !departureDate.value || !adultsNumber.value || !childrenNumber.value) {
+		evt.preventDefault();
+		modalWindow.classList.remove("modal-window-error");
+		modalWindow.offsetWidth = modalWindow.offsetWidth;
+		modalWindow.classList.add("modal-window-error");
+	}
+	else {
+		localStorage.setItem("adults", adultsNumber.value);
+		localStorage.setItem("children", childrenNumber.value);
+	}
+})
